323a9cfeb0abae0428387eb9c23c63c930171737
1t1sCooL <mmalabugin@gmail.com>
2025-07-19 00:13:27 +0300

    adaptive design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   <Head>
     <meta charSet="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <link rel="icon" type="image/png" sizes="32x32" href='./favicon-32x32.png'/>
+    <link rel="icon" type="image/png" sizes="32x32" href='./../../public/favicon-32x32.png'/>
     <title>Frontend Mentor | QR code component</title>
   </Head>
   <body>
