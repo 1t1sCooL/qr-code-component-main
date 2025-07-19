a997a5805bb58196fd4ac52163cbe5cea972d664
1t1sCooL <malabugin@anykey.pro>
2025-07-19 16:13:45 +0300

    finish

--- src/pages/qrPage/QRBody/QRImage.tsx
+++ src/pages/qrPage/QRBody/QRImage.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+// import { useRouter } from "next/router";
 
 export default function QRImage(){
+    // const { basePath } = useRouter();
+
     return (<div className="main-image">
         <Image
-            src="/image-qr-code.png"
+            // src={`${basePath}/image-qr-code.png`}
+            src='/image-qr-code.png'
             alt="QR Code"
             width={300}
             height={300}
--- src/pages/qrPage/QRHead.tsx
+++ src/pages/qrPage/QRHead.tsx
@@ -1,10 +1,17 @@
 import Head from "next/head";
+// import {useRouter} from "next/router";
 
 export default function QRHead(){
+
+    // const { basePath } = useRouter();
+
     return(<Head>
         <meta charSet="UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <link rel="icon" type="image/png"  href='/favicon-32x32.png'/>
+        <link rel="icon" type="image/png"
+              //href={`${basePath}/favicon-32x32.png`}
+              href='/favicon-32x32.png'
+        />
         <title>Frontend Mentor | QR code component</title>
     </Head>
         )
