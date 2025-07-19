c2e2b4701c6a8c361def4f2ea718d3339a67ab17
1t1sCooL <mmalabugin@gmail.com>
2025-07-19 13:01:47 +0300

    icon

--- src/pages/qrPage/QRHead.tsx
+++ src/pages/qrPage/QRHead.tsx
@@ -4,7 +4,7 @@ export default function QRHead(){
     return(<Head>
         <meta charSet="UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <link rel="icon" type="image/png" sizes="32x32" href='./../../../public/favicon-32x32.png'/>
+        <link rel="icon" type="image/png"  href='/favicon-32x32.png'/>
         <title>Frontend Mentor | QR code component</title>
     </Head>
         )
