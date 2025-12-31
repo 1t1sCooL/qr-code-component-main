35d74e5d2c241a72f403536417b6819fe7cfb95f
1t1sCooL <mmalabugin@gmail.com>
2025-12-31 05:50:25 +0300

    fix

--- src/pages/qrPage/QRHead.tsx
+++ src/pages/qrPage/QRHead.tsx
@@ -1,18 +1,20 @@
 import Head from "next/head";
-// import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 
-export default function QRHead(){
+export default function QRHead() {
+  const { basePath } = useRouter();
 
-    // const { basePath } = useRouter();
-
-    return(<Head>
-        <meta charSet="UTF-8"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <link rel="icon" type="image/png"
-              //href={`${basePath}/favicon-32x32.png`}
-              href='/favicon-32x32.png'
-        />
-        <title>Frontend Mentor | QR code component</title>
+  return (
+    <Head>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link
+        rel="icon"
+        type="image/png"
+        href={`${basePath}/favicon-32x32.png`}
+        //   href='/favicon-32x32.png'
+      />
+      <title>Frontend Mentor | QR code component</title>
     </Head>
-        )
+  );
 }
